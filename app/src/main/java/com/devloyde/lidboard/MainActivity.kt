775ab5b55d47f3a98e61467cf128a95be98310ec
Oiku <yunglordye@gmail.com>
2020-08-31 15:28:54 +0100

    Added: navigation components for navigation

--- app/src/main/java/com/devloyde/lidboard/MainActivity.kt
+++ app/src/main/java/com/devloyde/lidboard/MainActivity.kt
@@ -1,28 +1,18 @@
 package com.devloyde.lidboard
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.tabs.TabLayout
-import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import com.devloyde.lidboard.ui.ddd.SectionsPagerAdapter
+import androidx.databinding.DataBindingUtil
+import com.devloyde.lidboard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        val viewPager: ViewPager = findViewById(R.id.view_pager)
-        viewPager.adapter = sectionsPagerAdapter
-        val tabs: TabLayout = findViewById(R.id.tabs)
-        tabs.setupWithViewPager(viewPager)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        // Not necessary since the only view in it is the nav host fragment
+        // and nav graph already connected through xml
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
